d89d3c5ffb3f2bec2c4c9eda8a5ec7343ca5e6f5
Jaime Kumar <jaimelacree@gmail.com>
2023-05-17 16:04:50 +0100

    resize function 10

--- src/App.js
+++ src/App.js
@@ -155,7 +155,7 @@ function App() {
 
   // FUNCTIONS //
   function resize() {
-    $('.wholeApp').css('height', window.innerHeight + 'px');
+    // $('.wholeApp').css('height', window.innerHeight + 'px');
     $('.pages').css('height', window.innerHeight * 0.91 + 'px');
   }
 
